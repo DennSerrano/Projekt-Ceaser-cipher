3802866181dc6ae579f3f3e086da8af5659690e0
Dennserrano <dennserrano@gmail.com>
2021-11-29 11:41:15 +0100

    v1

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+let geheimtext = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+let input = document.getElementById("input")
+let output = document.getElementById("output")
+let kodieren = document.getElementById("kodieren")
+let dekodieren = document.getElementById("dekodieren")
+let num = document.getElementById("num")
+let check = document.getElementById("check")
+let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let check = () => {
+
+
+}
